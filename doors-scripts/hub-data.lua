ae326028a2294d4501f9e31e003b4561a9321fe0
Creepy <123327228+CreepyPSC@users.noreply.github.com>
2023-04-24 18:41:16 +1000

    Update hub-data.lua

--- doors-scripts/hub-data.lua
+++ doors-scripts/hub-data.lua
@@ -1,4 +1,3 @@
 local scriptVersion = 1.0;
 local scriptDiscord = "BHQD5vvNxs";
-local scriptKey = "w-creepy-on-top";
-return scriptVersion, scriptDiscord, scriptKey
+return scriptVersion, scriptDiscord
